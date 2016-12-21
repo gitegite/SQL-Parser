dc7f1a37d763097bbd06d708326cd71b5e3fea44
gitegite <gitegite@users.noreply.github.com>
2016-12-21 17:50:28 +0700

    update

--- ParserProvider.cs
+++ ParserProvider.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,40 @@ namespace SQLParserDB
 {
     public class ParserProvider
     {
+        string[] GetWhereClause(string p_command)
+        {
+            string temp = MySubString.SubString(p_command, p_command.IndexOf("where"), p_command.Length - 1);
+            temp = temp.Replace("where", "");
+            temp = temp.Replace(" ", "");
+            return temp.Split('=');
+
+        }
+        void SortMergeJoin(JoinSchema target, Schema table1, Schema table2, string attr1, string attr2)
+        {
+            table1.Data = new Dictionary<string, Dictionary<string, string>>(table1.Data.OrderBy(x => x.Value[attr1]).ToDictionary(x => x.Key, x => x.Value));
+            table2.Data = new Dictionary<string, Dictionary<string, string>>(table2.Data.OrderBy(x => x.Value[attr2]).ToDictionary(x => x.Key, x => x.Value));
+
+            foreach (var record in table1.Data)
+            {
+                
+            }
+            
+            Cartesian(target, table1, table2);
+            foreach (var record in target.Data.Reverse())
+            {
+                var xx = record.Value.ToLookup(x => x.Item1, x => x.Item2)[attr1];
+                var yy = record.Value.ToLookup(y => y.Item1, x => x.Item2)[attr2];
+                var table1Attribute = xx.ElementAt(0);
+                var table2Attribute = yy.Count() > 1 ? yy.ElementAt(1) : yy.ElementAt(0);
+                if (!table1Attribute.Equals(table2Attribute))
+                {
+                    target.Data.Remove(record.Key);
+                }
+
+            }
+
+        }
+
         bool ValidateValueAndType(string typeName, string value, int typeLength)
         {
             switch (typeName)
@@ -47,16 +81,15 @@ namespace SQLParserDB
             return MySubString.SubString(attribute, 0, attribute.IndexOf('(') - 1);
 
         }
-        void Cartesian(Schema target, Schema table1, Schema table2)
+        void Cartesian(JoinSchema target, Schema table1, Schema table2)
         {
-            var cartesianed = new Dictionary<string, Dictionary<string, string>>();
-            int surrogatekey = 0;
+            var cartesianed = new Dictionary<string, List<Tuple<string, string>>>();
+            int surrogatekey = 1;
             foreach (var record1 in table1.Data)
             {
                 var temp = new Dictionary<string, string>();
                 var temp2 = new Dictionary<string, string>();
-                var temp3 = new Dictionary<string, string>();
-
+                var temp3 = new List<Tuple<string, string>>();
                 foreach (var attribute in record1.Value)
                 {
                     temp.Add(attribute.Key, attribute.Value);
@@ -65,7 +98,8 @@ namespace SQLParserDB
                 {
                     foreach (var item in temp)
                     {
-                        temp3.Add(item.Key, item.Value);
+                        temp3.Add(new Tuple<string, string>(item.Key, item.Value));
+                        //temp3.Add(item.Key, item.Value);
                     }
                     foreach (var attribute2 in record2.Value)
                     {
@@ -74,10 +108,12 @@ namespace SQLParserDB
 
                     foreach (var item in temp2)
                     {
-                        temp3.Add(item.Key, item.Value);
+                        temp3.Add(new Tuple<string, string>(item.Key, item.Value));
+
+                        //temp3.Add(item.Key, item.Value);
                     }
 
-                    cartesianed.Add(surrogatekey.ToString(), new Dictionary<string, string>(temp3));
+                    cartesianed.Add(surrogatekey.ToString(), new List<Tuple<string, string>>(temp3));
                     surrogatekey++;
                     temp3.Clear();
                     temp2.Clear();
@@ -88,10 +124,12 @@ namespace SQLParserDB
                 target.Data.Add(item.Key, item.Value);
             }
         }
-        Schema _oldSchema = new Schema();
-        Schema _newSchema = new Schema();
+
         public string ParseSQLCommand(string p_command)
         {
+            Schema _oldSchema = new Schema();
+            Schema _newSchema = new Schema();
+            JoinSchema _joinSchema = new JoinSchema();
             string parseResult = "";
             string _tableName = "";
             string[] SplittedCommand = p_command.Split(' ');
@@ -127,6 +165,7 @@ namespace SQLParserDB
                                 fromTwoTables[i] = fromTwoTables[i].Trim();
                             }
                         }
+
                         if (File.Exists(Path.Combine(_dir, singleTableName + ".txt")) || File.Exists(Path.Combine(_dir, fromTwoTables[0] + ".txt")))
                         {
                             _newSchema = new Schema();
@@ -138,20 +177,32 @@ namespace SQLParserDB
                                 {
                                     Schema table1 = JsonConvert.DeserializeObject<Schema>(File.ReadAllText(Path.Combine(_dir, fromTwoTables[0] + ".txt")));
                                     Schema table2 = JsonConvert.DeserializeObject<Schema>(File.ReadAllText(Path.Combine(_dir, fromTwoTables[1] + ".txt")));
-                                    Cartesian(_newSchema, table1, table2);
-                                    parseResult = JsonConvert.SerializeObject(_newSchema);
+                                    //Cartesian(_newSchema, table1, table2);                                    
+                                    //parseResult = JsonConvert.SerializeObject(_newSchema);
+                                    Cartesian(_joinSchema, table1, table2);
+                                    parseResult = JsonConvert.SerializeObject(_joinSchema);
+
                                 }
                                 else
                                 {
                                     m_selected = File.ReadAllText(Path.Combine(_dir, singleTableName + ".txt"));
-                                    parseResult = m_selected;
+                                    var q = JsonConvert.DeserializeObject<Schema>(m_selected);
+                                    foreach (var item in q.Data)
+                                    {
+                                        _joinSchema.Data.Add(item.Key, new List<Tuple<string, string>>());
+                                        foreach (var item2 in item.Value)
+                                        {
+                                            _joinSchema.Data[item.Key].Add(new Tuple<string, string>(item2.Key, item2.Value));
+
+                                        }
+                                    }
+                                    parseResult = JsonConvert.SerializeObject(_joinSchema);
                                 }
                             }
                             else
                             {
-                                string whereClause = MySubString.SubString(p_command, p_command.IndexOf("where"), p_command.Length - 1);
-                                whereClause = whereClause.Replace("where", "");
-                                string[] whereClauseKeyValue = whereClause.Split('=');
+                                string[] whereClauseKeyValue = new string[2];
+                                whereClauseKeyValue = GetWhereClause(p_command);
 
                                 if (fromTwoTables[1] == null)
                                 {
@@ -160,13 +211,19 @@ namespace SQLParserDB
 
                                     foreach (var record in _oldSchema.Data)
                                     {
+                                        List<Tuple<string, string>> temp = new List<Tuple<string, string>>();
                                         foreach (var attribute in record.Value)
                                         {
                                             if (attribute.Key == whereClauseKeyValue[0].Trim() && attribute.Value == whereClauseKeyValue[1].Trim().Replace("'", ""))
                                             {
-                                                _newSchema.Data.Add(record.Key, record.Value);
+                                                foreach (var item in record.Value)
+                                                {
+                                                    temp.Add(new Tuple<string, string>(item.Key, item.Value));
+                                                }
+
                                             }
                                         }
+                                        _joinSchema.Data.Add(record.Key, temp);
                                     }
                                 }
                                 else
@@ -174,17 +231,29 @@ namespace SQLParserDB
                                     fromTwoTables[1] = MySubString.SubString(fromTwoTables[1], 0, fromTwoTables[1].IndexOf(' ') - 1);
                                     Schema table1 = JsonConvert.DeserializeObject<Schema>(File.ReadAllText(Path.Combine(_dir, fromTwoTables[0] + ".txt")));
                                     Schema table2 = JsonConvert.DeserializeObject<Schema>(File.ReadAllText(Path.Combine(_dir, fromTwoTables[1] + ".txt")));
-                                    Cartesian(_newSchema, table1, table2);
-                                    foreach (var record in _newSchema.Data.Reverse())
-                                    {
-                                        if (record.Value.Any(x => x.Key == whereClauseKeyValue[0].Trim() && x.Value != whereClauseKeyValue[1].Trim().Replace("'", "")))
-                                        {
-                                            _newSchema.Data.Remove(record.Key);
-                                        }
 
+                                    //Cartesian(_joinSchema, table1, table2);
+                                    if (whereClauseKeyValue.Any(x => x.Contains(".")))
+                                    {
+                                        var table1WhereAttribute = whereClauseKeyValue[0].Split('.');
+                                        var table2WhereAttribute = whereClauseKeyValue[1].Split('.');
+                                        SortMergeJoin(_joinSchema, table1, table2, table1WhereAttribute[1], table2WhereAttribute[1]);
+                                        
+                                    }
+                                    else
+                                    {
+                                        //WRECKED
+                                        foreach (var record in _newSchema.Data.Reverse())
+                                        {
+                                            if (record.Value.Any(x => x.Key == whereClauseKeyValue[0].Trim() && x.Value != whereClauseKeyValue[1].Trim().Replace("'", "")))
+                                            {
+                                                _joinSchema.Data.Remove(record.Key);
+                                            }
+
+                                        }
                                     }
                                 }
-                                parseResult = JsonConvert.SerializeObject(_newSchema);
+                                parseResult = JsonConvert.SerializeObject(_joinSchema);
 
                             }
                         }
@@ -197,6 +266,7 @@ namespace SQLParserDB
                     {
                         string m_selectedField = MySubString.SubString(p_command, p_command.IndexOf("select") + 7, p_command.LastIndexOf("from") - 2);
                         string[] selectedFieldsArray = m_selectedField.Split(',');
+
                         selectedFieldsArray = selectedFieldsArray.Select(x => x.Trim()).ToArray();
                         _tableName = MySubString.SubString(p_command, p_command.LastIndexOf("from") + 5, p_command.Length - 1);
 
