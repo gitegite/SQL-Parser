92c19e50139057c3f64446ae9607e7eb70fef38b
gitegite <gitegite@users.noreply.github.com>
2016-12-21 17:51:28 +0700

    Update

--- Schema.cs
+++ Schema.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,12 +13,15 @@ namespace SQLParserDB
         public string TableName { get; set; }
         public Dictionary<string,string> Attributes { get; set; }
         public Dictionary<string, Dictionary<string,string>> Data { get; set; }
+        public const int EndPosition = -1;
+        public int Position { get; set; }
 
         public Schema()
         {
             TableName = "";
             Attributes = new Dictionary<string, string>();
             Data = new Dictionary<string, Dictionary<string,string>>();
+            
         }
 
         public Schema(string m_tableNameC)
@@ -26,6 +29,19 @@ namespace SQLParserDB
             TableName = m_tableNameC;
             Attributes = new Dictionary<string, string>();
             Data = new Dictionary<string, Dictionary<string,string>>();
+            Position = 0;
+        }
+
+        public bool IsDone() { return Position == EndPosition; }
+        public bool Advance()
+        {
+            if (Position == Data.Count - 1 || Position == EndPosition)
+            {
+                Position = EndPosition;
+                return false;
+            }
+            Position++;
+            return true;
         }
 
     }
